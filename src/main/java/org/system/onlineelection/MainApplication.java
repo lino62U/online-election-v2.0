52448348e59c210c98f6851db5625c75432def14
leonf <ldavis@unsa.edu.pe>
2023-12-18 01:26:43 -0500

    test2

--- src/main/java/org/system/onlineelection/MainApplication.java
+++ src/main/java/org/system/onlineelection/MainApplication.java
@@ -2,10 +2,8 @@ package org.system.onlineelection;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"org.system.onlineelection"})
 public class MainApplication {
 
 	public static void main(String[] args) {
